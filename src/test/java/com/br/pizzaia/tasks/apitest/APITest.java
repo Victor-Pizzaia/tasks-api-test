7ec87981a153c5d0a3536a0e427b5b94a8977e43
Victor-Pizzaia <v_pizzaia@hotmail.com>
2022-04-23 11:22:53 -0300

    initial commit

---
+++ src/test/java/com/br/pizzaia/tasks/apitest/APITest.java
@@ -0,0 +1,51 @@
+package com.br.pizzaia.tasks.apitest;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class APITest {
+	
+	@BeforeClass
+	public static void setup() {
+		RestAssured.baseURI = "http://localhost:8001/tasks-backend";
+	}
+
+	@Test
+	public void shouldReturnTasks() {
+		RestAssured.given()
+		.when()
+			.get("/todo")
+		.then()
+			.statusCode(200)
+		;
+	}
+	
+	@Test
+	public void shouldAddTaskWithSuccess() {
+		RestAssured.given()
+			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2022-04-30\" }")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(201)
+		;
+	}
+	
+	@Test
+	public void shouldNotAddInvalidTask() {
+		RestAssured.given()
+			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2010-04-30\" }")
+			.contentType(ContentType.JSON)
+		.when()
+			.post("/todo")
+		.then()
+			.statusCode(400)
+			.body("message", CoreMatchers.is("Due date must not be in past"))
+		;
+	}
+}
